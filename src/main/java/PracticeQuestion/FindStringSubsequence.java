ffe957902d5750f43ab4cde8c50011ddfdfcf172
Mayur Dharne <mayurdharane@gmail.com>
2021-05-30 12:23:53 +0530

    some more practice

---
+++ src/main/java/PracticeQuestion/FindStringSubsequence.java
@@ -0,0 +1,69 @@
+package PracticeQuestion;
+
+import java.io.Console;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+
+public class FindStringSubsequence {
+
+    /**
+     * Sample input : coronavirus 4 abcde crnas onarous ooaius
+     * @param args
+     */
+
+    public static void main (String [] args){
+
+        Scanner in = new Scanner(System.in);
+        String mainString = in.next();
+        String sub1 = "";
+
+//        Integer num = Integer.valueOf(noOfSubString);
+        int length = Integer.valueOf(in.next());
+        for (int argNum = 0; argNum < length; argNum++) {
+
+            sub1 = in.next();//args[argNum];
+            int foundSubStringlegth = sub1.length();
+
+            char[] array = mainString.toCharArray();
+            char[] subArray = sub1.toCharArray();
+
+//            int iStart = 0;
+            int jStart = 0;
+
+            for (int i = 0; i < subArray.length; i++) {
+                char c = subArray[i];
+
+                if(foundSubStringlegth == 0) break;
+
+                for (int j = jStart; j < array.length; j++) {
+
+                    if (c == array[j]) {
+//                        found[i] = c;
+                        jStart = ++j;
+                        foundSubStringlegth--;
+//                        System.out.println(" found :" + Arrays.toString(found));
+                        break;
+                    }
+
+                }
+            }
+            if(foundSubStringlegth == 0){
+                System.out.println("POSITIVE");
+
+            }else{
+                System.out.println("NEGATIVE");
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- src/main/java/dateTimeApis/dateTimeApis.java
+++ src/main/java/dateTimeApis/dateTimeApis.java
@@ -4,6 +4,7 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class dateTimeApis {
 
@@ -19,34 +20,58 @@ public class dateTimeApis {
         LocalDateTime berlinTime = LocalDateTime.now(berlinZone);
         System.out.println("Berlin :"+ berlinTime);
 
-        ZoneOffset berlinOffset = berlinZone.getRules().getOffset(indiaTime);
-        System.out.println("Berlin Timezone offset : "+berlinOffset);
+//        ZoneOffset berlinOffset = berlinZone.getRules().getOffset(indiaTime);
+//        System.out.println("Berlin Timezone offset : "+berlinOffset);
+//
+//        ZoneOffset indiaOffset = indiaTimeZone.getRules().getOffset(indiaTime);
+//        System.out.println("india TimeZone offset : "+indiaOffset);
 
-        ZoneOffset indiaOffset = indiaTimeZone.getRules().getOffset(indiaTime);
-        System.out.println("india TimeZone offset : "+indiaOffset);
+//        OffsetDateTime indiaTimeOffsetToUTC = OffsetDateTime.of(indiaTime, indiaOffset);
+//        System.out.println("India offsetDateTime : "+indiaTimeOffsetToUTC);
+//
+//        OffsetDateTime berlinTimeOffsetToUTC = OffsetDateTime.of(berlinTime, berlinOffset);
+//        System.out.println("Berlin offsetDateTime : "+berlinTimeOffsetToUTC);
+//
+//        System.out.println("atZoneSameInstant :"+berlinTimeOffsetToUTC.atZoneSameInstant(indiaTimeZone));
 
-        OffsetDateTime indiaTimeOffsetToUTC = OffsetDateTime.of(indiaTime, indiaOffset);
-        System.out.println("India offsetDateTime : "+indiaTimeOffsetToUTC);
-
-        OffsetDateTime berlinTimeOffsetToUTC = OffsetDateTime.of(berlinTime, berlinOffset);
-        System.out.println("Berlin offsetDateTime : "+berlinTimeOffsetToUTC);
-
-        System.out.println("atZoneSameInstant :"+berlinTimeOffsetToUTC.atZoneSameInstant(indiaTimeZone));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        LocalDateTime xmldateTime = LocalDateTime.parse("2021-05-20T02:30:30", formatter);
+        System.out.println("xmldateTime :"+xmldateTime);
 
         // what the time for a specific zone or offset is in our own timezone
-        ZonedDateTime indiaZonedDateTime = LocalDateTime.now().atZone(indiaTimeZone);
+        ZonedDateTime indiaZonedDateTime = xmldateTime.atZone(indiaTimeZone); //LocalDateTime.now()
         System.out.println("India zonedDateTime :"+indiaZonedDateTime);
+        System.out.println("India dateTime :"+indiaZonedDateTime.toLocalDateTime());
         ZonedDateTime berlinZonedDateTime1 = indiaZonedDateTime.withZoneSameInstant(berlinZone);
         System.out.println("Berlin zonedDateTime :"+ berlinZonedDateTime1);
+        System.out.println("Berlin dateTime :"+ berlinZonedDateTime1.toLocalDateTime());
 
 
-        ConversionService conversionService = new DefaultConversionService();
-        LocalDateTime localDateTime1 = conversionService.convert(berlinZonedDateTime1, LocalDateTime.class);
-        System.out.println("localDateTime1 :"+localDateTime1);
+//        ConversionService conversionService = new DefaultConversionService();
+//        LocalDateTime localDateTime1 = conversionService.convert(berlinZonedDateTime1, LocalDateTime.class);
+//        System.out.println("localDateTime1 :"+localDateTime1);
+//
+//        System.out.println("--------------");
+//        LocalDateTime oldDateTime = LocalDateTime.parse("2021-12-05T17:31:00");
+//        ZoneId oldZone = ZoneId.of("America/Chicago");
+//        System.out.println("oldDateTime : "+oldDateTime);
+//        ZoneId newZone = ZoneId.of("America/New_York");
+//        LocalDateTime newDateTime = oldDateTime.atZone(oldZone)
+//                .withZoneSameInstant(newZone)
+//                .toLocalDateTime();
+//        System.out.println(newDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+//        System.out.println("--------------");
+//        System.out.println("indiaTime : "+indiaTime);
+//        LocalDateTime toLocalDateTime = indiaTime.atZone(indiaTimeZone)
+//                .withZoneSameInstant(oldZone)
+//                .toLocalDateTime();
+//
+//        System.out.println("Ind toLocalDateTime in Berlin : "+toLocalDateTime);
+//
+//        System.out.println("***********************************");
+//
+//        System.out.println("indiaTime : "+indiaTime);
 
-
-        LocalDateTime localDateTime = LocalDateTime.from(berlinTimeOffsetToUTC);
-        System.out.println(localDateTime);
 
 
 
